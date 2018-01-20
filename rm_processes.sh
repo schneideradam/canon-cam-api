2a7d7b02f599fd4cd08c927a92c9bc4466a7b0d5
Adam Schneider <adam.schneider@spinifexgroup.com>
2018-01-20 19:24:10 +0000

    adding special volume handler

---
+++ rm_processes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo rm /usr/share/dbus-1/services/org.gtk.vfs.GPhoto2VolumeMonitor.service
+sudo rm /usr/share/gvfs/mounts/gphoto2.mount
+sudo rm /usr/share/gvfs/remote-volume-monitors/gphoto2.monitor
+sudo rm /usr/lib/gvfs/gvfs-gphoto2-volume-monitor
